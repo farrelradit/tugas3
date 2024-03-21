a3ca7bff1913ec83fc038925171e6d47274982ef
farrelradit <142956704+farrelradit@users.noreply.github.com>
2024-03-21 19:43:59 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,33 @@
+package tugas3;
+
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private int nilaiAngka;
+    private String nilaiHuruf;
+    
+    public String getNilaiHuruf() {
+        return nilaiHuruf;
+    }
+    public void setNilaiHuruf(String nilaiHuruf) {
+        this.nilaiHuruf = nilaiHuruf;
+    }
+    public int getNilaiAngka() {
+        return nilaiAngka;
+    }
+    public void setNilaiAngka(int nilaiAngka) {
+        this.nilaiAngka = nilaiAngka;
+    }
+    public String getNim() {
+        return nim;
+    }
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
